e989a72748dafa05f28bde0412f6df4d288e1e10
Caroline Lynch <carolineablynch@gmail.com>
2021-02-08 19:14:44 -0500

    creating roles SQL table and running ttable in SQL. Also moved around the tables

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -4,4 +4,28 @@ CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
-CREATE TABLE employee( )
+CREATE TABLE department (
+id INT(1) AUTO_INCREMENT NOT NULL,
+department_name VARCHAR(30) NULL,
+PRIMARY KEY (id)
+); 
+
+
+CREATE TABLE roles (
+id INT(1) AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NULL,
+salary DECIMAL(10, 2) NULL,
+department_id INT(10) NULL, 
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee ( 
+id INT(1) AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NULL, 
+last_name VARCHAR(30) NULL,
+role_id INT(10) NOT NULL,
+manager_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES roles(id)
+); 
